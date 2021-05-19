f17c2b25b4a178153947aeb1f89c4340b49a5bd3
Pavel.Zagainov <pasha_zigzag@mail.ru>
2021-05-20 09:24:22 +1000

    v2

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
-$dir = './';
 
-function getCountSum($dir) : int
-{
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-    $sum = 0;
+require 'CountFinder.php';
 
-    foreach ($iterator as $path) {
-        if(is_file($path->getPathName()) && $path->getFileName() === 'count') {
-            $count = fopen($path->getPathName(), 'r');
-            while($line = fgets($count)) {
-                $number = intval($line);
-                $sum += $number;
-            }
+$countFinder = new CountFinder('./', '');
 
-            fclose($count);
-        }
-    }
-
-    return $sum;
+try {
+    $countFinder->calculateCountSum();
+} catch (Exception $e) {
+    die($e->getMessage());
 }
 
-echo getCountSum($dir);
+echo $countFinder->getCountSum();
